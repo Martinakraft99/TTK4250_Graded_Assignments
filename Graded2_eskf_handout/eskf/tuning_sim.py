f85bd2aecc2fc2ded5ecdb1c1966ef30248454ff
Martinakraft99 <martiakr@stud.ntnu.no>
2021-11-05 13:15:52 +0100

    first attempt

--- Graded2_eskf_handout/eskf/tuning_sim.py
+++ Graded2_eskf_handout/eskf/tuning_sim.py
@@ -3,23 +3,32 @@ from quaternion import RotationQuaterion
 from datatypes.eskf_params import ESKFTuningParams
 from datatypes.eskf_states import NominalState, ErrorStateGauss
 
+deg2rad = 3.1415/180
+
+vel_rand_walk   = 0.07
+acc_rand_walk   = 0.04
+ang_rand_walk   = 0.15
+rate_rand_walk  = 0.9
+accm_bias_insta = 0 #0.05
+gyro_bias_insta = 0 #0.5
+
 tuning_params_sim = ESKFTuningParams(
-    accm_std=1.,
-    accm_bias_std=1.,
-    accm_bias_p=1.,
+    accm_std =      (vel_rand_walk * 60)*1 ,                 
+    accm_bias_std=  (acc_rand_walk * np.sqrt(3/5000) + accm_bias_insta) * 9810,    
+    accm_bias_p=    0.5/10000.,
 
-    gyro_std=1.,
-    gyro_bias_std=1.,
-    gyro_bias_p=1.,
+    gyro_std =      (ang_rand_walk * deg2rad*3600)*0.5,        
+    gyro_bias_std = rate_rand_walk * deg2rad/216000 + gyro_bias_insta * deg2rad/3600, 
+    gyro_bias_p =   0.5/10000.,
 
-    gnss_std_ne=1.,
-    gnss_std_d=1.)
+    gnss_std_ne =   1,
+    gnss_std_d =    5)
 
 x_nom_init_sim = NominalState(
-    np.array([0., 0., 0.]),  # position
+    np.array([0, 0., 0.]),  # position
     np.array([0., 0., 0.]),  # velocity
     RotationQuaterion.from_euler([0., 0., 0.]),  # orientation
-    np.zeros(3),  # accelerometer bias
+    np.ones(3),  # accelerometer bias
     np.zeros(3),  # gyro bias
     ts=0.)
 
