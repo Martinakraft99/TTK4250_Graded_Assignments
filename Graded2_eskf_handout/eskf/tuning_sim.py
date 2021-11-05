9e6cb9ce8ce9a5ee190dd1a0377b0713317d923f
Magnus Mæhlum <magnusmaehlum@outlook.com>
2021-11-05 16:40:52 +0100

    Change config; add good parameters to runing_sim.py

--- Graded2_eskf_handout/eskf/tuning_sim.py
+++ Graded2_eskf_handout/eskf/tuning_sim.py
@@ -13,12 +13,13 @@ accm_bias_insta = 0.05
 gyro_bias_insta = 0.03 #0.5
 
 tuning_params_sim = ESKFTuningParams(
-    accm_std =      2*(vel_rand_walk / 60) ,                 
-    accm_bias_std=  (acc_rand_walk * np.sqrt(3/5000) + accm_bias_insta)/100,    
+    accm_std =      2 * (vel_rand_walk / 60) ,                 
+    accm_bias_std=  5 * (acc_rand_walk * np.sqrt(3/5000) + accm_bias_insta) / 100,    
     accm_bias_p=    0.5/10000.,
 
-    gyro_std =      2*(ang_rand_walk * deg2rad/60),        
-    gyro_bias_std = 0.3*((rate_rand_walk * deg2rad/(60**3))+ gyro_bias_insta * deg2rad/(60**2)), 
+    gyro_std =      2 * (ang_rand_walk * deg2rad / 60),        
+    gyro_bias_std = 30 * ((rate_rand_walk * deg2rad / (60 ** 3)) 
+                        + gyro_bias_insta * deg2rad / (60 ** 2)), 
     gyro_bias_p =   0.5/10000.,
 
     gnss_std_ne =   0.45,
@@ -35,7 +36,7 @@ x_nom_init_sim = NominalState(
 init_std_sim = np.repeat(repeats=3,  # repeat each element 3 times
                          a=[1.,  # position
                             1.,  # velocity
-                            np.deg2rad(10),  # angle vector
+                            np.deg2rad(1),  # angle vector
                             0.1,  # accelerometer bias
-                            .001])  # gyro bias
+                            .01])  # gyro bias
 x_err_init_sim = ErrorStateGauss(np.zeros(15), np.diag(init_std_sim**2), 0.)
