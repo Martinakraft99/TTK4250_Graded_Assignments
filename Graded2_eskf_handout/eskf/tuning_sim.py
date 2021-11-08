63d397e84e69b2257c8ff7647654da4608c08af0
Magnus Mæhlum <magnusmaehlum@outlook.com>
2021-11-08 15:23:21 +0100

    Fix tuning in sim.py

--- Graded2_eskf_handout/eskf/tuning_sim.py
+++ Graded2_eskf_handout/eskf/tuning_sim.py
@@ -1,34 +1,37 @@
 import numpy as np
+import math
 from quaternion import RotationQuaterion
 from datatypes.eskf_params import ESKFTuningParams
 from datatypes.eskf_states import NominalState, ErrorStateGauss
 
-deg2rad = 3.1415/180
+deg2rad = math.pi/180
 
-vel_rand_walk   = 0.07                
-acc_rand_walk   = 0.04
-ang_rand_walk   = 0.15
-rate_rand_walk  = 0.9
-accm_bias_insta = 0.05
-gyro_bias_insta = 0.03 #0.5
+vel_rand_walk   = 0.07    # datasheet            
+acc_rand_walk   = 0.001   # Allan deviations
+ang_rand_walk   = 0.15    # datasheet
+rate_rand_walk  = 0.9     # Allan deviations
+accm_bias_insta = 0.05    # datasheet
+gyro_bias_insta = 0.5     # datasheet 
+
+p_common = 0.5/10000
 
 tuning_params_sim = ESKFTuningParams(
-    accm_std =      2 * (vel_rand_walk / 60) ,                 
-    accm_bias_std=  5 * (acc_rand_walk * np.sqrt(3/5000) + accm_bias_insta) / 100,    
-    accm_bias_p=    0.5/10000.,
+    accm_std =      2 * (vel_rand_walk / 60),
+    accm_bias_std=  5 * (acc_rand_walk + accm_bias_insta) / 100,
+    accm_bias_p=    p_common,
 
-    gyro_std =      2 * (ang_rand_walk * deg2rad / 60),        
-    gyro_bias_std = 30 * ((rate_rand_walk * deg2rad / (60 ** 3)) 
-                        + gyro_bias_insta * deg2rad / (60 ** 2)), 
-    gyro_bias_p =   0.5/10000.,
+    gyro_std =      2 * (ang_rand_walk * deg2rad / 60),
+    gyro_bias_std = 3 * ((rate_rand_walk * deg2rad / (60 ** 3))
+                        + gyro_bias_insta * deg2rad / (60 ** 2)),
+    gyro_bias_p =   p_common,
 
-    gnss_std_ne =   0.8 * 0.45,
-    gnss_std_d =    0.8 * 0.7)
+    gnss_std_ne =   0.4,
+    gnss_std_d =    0.45)
 
 x_nom_init_sim = NominalState(
     np.array([0.2, 0., -5.]),  # position
     np.array([20., 0., 0.]),  # velocity
-    RotationQuaterion.from_euler([0., 0., 0.]),  # orientation
+    RotationQuaterion.from_euler([0., 0., 0]),  # orientation
     np.zeros(3),  # accelerometer bias
     np.zeros(3),  # gyro bias
     ts=0.)
